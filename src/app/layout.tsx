2dcafea880cce6bfcc741b186258d1dc9dad8408
Boris <boris.vicena@gmail.com>
2026-02-01 00:02:03 +0100

    Add GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Nav } from "../components/nav";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,6 +58,7 @@ export default function RootLayout({
           <main>{children}</main>
         </div>
       </body>
+      <GoogleAnalytics gaId="G-N006CLMG44" />
     </html>
   );
 }
